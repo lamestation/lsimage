fdf9cab894491acc63a19d008c6e81b0838749c3
Brett Weir <brett@lamestation.com>
2016-03-19 15:50:16 -0700

    Add file export and blank opening image.

--- src/imageconverterdialog.cpp
+++ src/imageconverterdialog.cpp
@@ -3,14 +3,17 @@
 #include <QDebug>
 #include <QFileDialog>
 #include <QFileInfo>
+#include <QMessageBox>
 
 ImageConverterDialog::ImageConverterDialog(QWidget *parent)
     : QWidget(parent)
 {
     ui.setupUi(this);
 
-    connect(ui.openButton, SIGNAL(clicked()), this, SLOT(open()));
-//    connect(ui.exportButton, SIGNAL(triggered()), this, SLOT(save()));
+    connect(ui.openButton,  SIGNAL(clicked()), this, SLOT(open()));
+    connect(ui.exportButton,SIGNAL(clicked()), this, SLOT(exportFile()));
+
+    newFile();
 
 }
 
@@ -28,6 +31,13 @@ void ImageConverterDialog::open()
         openFile(fn);
 }
 
+void ImageConverterDialog::newFile()
+{
+    QImage blank(QSize(128,128), QImage::Format_RGB32);
+    blank.fill(Qt::white);
+    _converter.loadImage(blank);
+    updateImage();
+}
 
 void ImageConverterDialog::openFile(QString name)
 {
@@ -36,6 +46,8 @@ void ImageConverterDialog::openFile(QString name)
         qDebug() << "File does not exist";
         return;
     }
+
+    _filename = name;
 
     QImage img(name);
 
@@ -51,23 +63,60 @@ void ImageConverterDialog::openFile(QString name)
 
     qDebug() << framew << frameh;
 
-    ImageConverter imageConverter(img);
 
-    imageConverter.setColorTable("whiteblue");
-//    imageConverter.setFrameSize(framew, frameh);
-    imageConverter.setScaleFactor(4.0);
-    imageConverter.setDynamicRange();
-    imageConverter.recolor();
+    _converter.loadImage(img);
+    _converter.setColorTable("whiteblue");
+//    _converter.setFrameSize(framew, frameh);
+    _converter.setScaleFactor(4.0);
+    _converter.setDynamicRange();
+    _converter.recolor();
 
+    updateImage();
+}
+
+void ImageConverterDialog::updateImage()
+{
     ui.originalImage->setPixmap(
             QPixmap::fromImage(
-                imageConverter.originalImage()
+                _converter.originalImage()
             )
         );
 
     ui.resultImage->setPixmap(
             QPixmap::fromImage(
-                imageConverter.resultImage()
+                _converter.resultImage()
             )
         );
+}
+
+void ImageConverterDialog::exportFile()
+{
+    QFileInfo outfi(_filename);
+    QString outfilename = outfi.path() + "/gfx_"+outfi.completeBaseName()+".spin";
+
+    exportFile(outfilename);
+}
+
+void ImageConverterDialog::exportFile(QString name)
+{
+    if (QFileInfo(name).exists())
+    {
+        QMessageBox::StandardButton reply;
+        reply = QMessageBox::question(this, tr("Overwrite existing file?"), tr("%1 already exists. Do you want to overwrite it?").arg(name),
+                QMessageBox::Yes|QMessageBox::No);
+        if (reply == QMessageBox::No)
+            return;
+    }
+
+    QString output = _converter.toSpin(_filename);
+
+    QFile file(name);
+    if (file.open(QIODevice::WriteOnly))
+    {
+        QTextStream outstream(&file);
+        outstream << output;
+    }
+    file.close();
+
+    qDebug() << "Saved to" << name;
 }
--- src/imageconverterdialog.h
+++ src/imageconverterdialog.h
@@ -10,14 +10,19 @@ class ImageConverterDialog : public QWidget
 {
     Q_OBJECT
 
-private:
     Ui::ImageConverterDialog ui;
+    QString _filename;
+    ImageConverter _converter;
 
 public:
     explicit ImageConverterDialog(QWidget *parent = 0);
     ~ImageConverterDialog();
 
 public slots:
+    void updateImage();
+    void newFile();
     void open();
     void openFile(QString name);
+    void exportFile();
+    void exportFile(QString name);
 };
